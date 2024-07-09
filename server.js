bec5ac2fe20f5d4a78d92fd630dffce7fdf38a16
Smile <singhswapnil060303@gmail.com>
2024-07-09 15:41:46 +0530

    chore: Add routes for authentication, items, and rentals

--- server.js
+++ server.js
@@ -7,9 +7,17 @@ dotenv.config();
 
 const app = express();
 
-Middleware
+//Middleware
 app.use(cors());
 app.use(express.json());
+const authRoutes = require('./routes/auth');
+const itemRoutes = require('./routes/items');
+const rentalRoutes = require('./routes/rentals');
+
+app.use('/api/auth', authRoutes);
+app.use('/api/items', itemRoutes);
+app.use('/api/rentals', rentalRoutes);
+
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
