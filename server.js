e5e19e24166de2f500e3f23780e7d5037c20007b
Smile <singhswapnil060303@gmail.com>
2024-07-09 17:33:18 +0530

    chore: Add production build configuration for serving static files

--- server.js
+++ server.js
@@ -30,3 +30,14 @@ app.get('/', (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+
+const path = require('path');
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client/build')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
